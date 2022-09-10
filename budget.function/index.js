eb978a4d10b0f7e748a89de85717110baf01a8cb
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2022-09-09 22:02:02 -0400

    use complete() callback

--- budget.function/index.js
+++ budget.function/index.js
@@ -2,13 +2,14 @@ const Firestore = require('@google-cloud/firestore');
 
 const firestore = new Firestore();
 
-async function doit(event) {
+async function doit(event, complete) {
    await firestore.doc('monitoring/budget').set({
       cost: JSON.parse(Buffer.from(event.data, 'base64').toString()).costAmount,
       time: Firestore.Timestamp.now()
     });
+   complete();
 }
 
-exports.budgetPubSub = (event, context) => {
-   doit(event);
+exports.budgetPubSub = (event, context, complete) => {
+   doit(event, complete);
 };
