01a167fc3d97b0b05b60408efed4dae0fab6ea26
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2023-01-27 16:23:35 -0500

    improve startup reliability
    * systemd units for network & runner really should be "oneshot"
    * wait for network interface to indicate "up" before using the network
    * simplify selection of runner release to use (just use "latest")
    * don't pipe the runner download directly to tar: hopefully it can be retried more reliably that way

--- runner.vm/files/usr/local/bin/runner.sh
+++ runner.vm/files/usr/local/bin/runner.sh
@@ -7,9 +7,11 @@ GITHUB_RUNNER_REG_URL=$(${CURL} "http://metadata.google.internal/computeMetadata
 GITHUB_RUNNER_TOKEN=$(${CURL} "http://metadata.google.internal/computeMetadata/v1/instance/attributes/runnerToken" -H "Metadata-Flavor: Google")
 GITHUB_RUNNER_LABEL=$(${CURL} "http://metadata.google.internal/computeMetadata/v1/instance/attributes/runnerLabel" -H "Metadata-Flavor: Google")
 
-RUNNER_RELEASE_URL=$(${CURL} https://api.github.com/repos/actions/runner/releases | \
-	             jq -r 'first(.[] | select(.prerelease == false)) | .assets[] | if .name | test("actions-runner-linux-x64-[0-9.]+.tar") then .browser_download_url else empty end')
-${CURL} "${RUNNER_RELEASE_URL}" | tar zx
+RUNNER_RELEASE_URL=$(${CURL} https://api.github.com/repos/actions/runner/releases/latest | \
+                     jq -e -r '.assets[] | if .name | test("actions-runner-linux-x64-[0-9.]+.tar") then .browser_download_url else empty end')
+${CURL} -O "${RUNNER_RELEASE_URL}"
+tar xf *.tar.*
+rm *.tar.*
 
-./config.sh --url ${GITHUB_RUNNER_REG_URL} --token ${GITHUB_RUNNER_TOKEN} --ephemeral --disableupdate --unattended --labels ${GITHUB_RUNNER_LABEL}
+./config.sh --url "${GITHUB_RUNNER_REG_URL}" --token "${GITHUB_RUNNER_TOKEN}" --ephemeral --disableupdate --unattended --labels "${GITHUB_RUNNER_LABEL}"
 ./run.sh
